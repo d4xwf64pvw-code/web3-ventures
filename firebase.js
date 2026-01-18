658854d9d95cd766bf87dc0e0e114fc994514a93
d4xwf64pvw-code <d4xwf64pvw@privaterelay.appleid.com>
2026-01-18 04:54:07 +0100

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAQgPnlH68jZdivazaYqs0qTh7ejVbfmq4",
+    authDomain: "web3-network-b87c2.firebaseapp.com",
+    projectId: "web3-network-b87c2",
+    storageBucket: "web3-network-b87c2.firebasestorage.app",
+    messagingSenderId: "832614644635",
+    appId: "1:832614644635:web:36271b0b16b81a9ba0bf3e"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
